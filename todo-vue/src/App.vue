db8d46f7bb05d0684cc4f2151a2305e934854ab2
Diogo Belo <diogobelodss22@gmail.com>
2025-07-04 11:08:51 -0300

    Finalizando o projeto.

--- todo-vue/src/App.vue
+++ todo-vue/src/App.vue
@@ -1,106 +1,63 @@
 <script setup>
 import { reactive } from 'vue';
-
+import Cabecalho from './components/cabecalho.vue';
+import Formulario from './components/Formulario.vue';
+import ListaDeTarefas from './components/ListaDeTarefas.vue';
 const estado = reactive({
   filtro: 'todas',
-  tarefaTemp: '',
+  tarefaTemp: '' ,
   tarefas: [
     {
-      titulo: 'Estudar ES6',
-      concluida: false,
+      titulo: "Estudar ES6",
+      finalizada: false,
     },
     {
       titulo: 'Estudar SASS',
-      concluida: false,
+      finalizada: false
     },
     {
-      titulo: 'Ir para academia',
-      concluida: true,
+      titulo: "Ir para academia ",
+      finalizada: true,
     }
   ]
-})
+}) 
 
 const getTarefasPendentes = () => {
- return estado.tarefas.filter(tarefas => !tarefas.concluida)
+  return estado.tarefas.filter(tarefa => !tarefa.finalizada)
 }
 
-const getTarefasConcluidas = () => {
- return estado.tarefas.filter(tarefas => tarefas.concluida)
+const getTarefasFinalizadas = () => {
+  return estado.tarefas.filter(tarefa => tarefa.finalizada)
 }
 
-const getTarefasFiltradas = () => {
+const  getTarefasFiltradas =() =>{
   const {filtro} = estado;
 
-  switch (filtro) {
-    case 'pendentes' :
-      return getTarefasPendentes();
-    case 'concluida' :
-      return getTarefasConcluidas();
+  switch(filtro) {
+    case'pendentes':
+    return getTarefasPendentes();
+    case'finalizadas':
+    return getTarefasFinalizadas();
     default:
       return estado.tarefas;
   }
-}
+};
 
-const cadastraTarefa = () => {
+const cadastraTarefa = () =>{
   const tarefaNova = {
-    titulo: estado.tarefaTemp,
-    concluida: false,
+    titulo:estado.tarefaTemp,
+    finalizada:false,
   }
-  estado.tarefas.push(tarefaNova);
-  estado.tarefaTemp = '';
+  estado.tarefas.push(tarefaNova)
+  estado.tarefaTemp= '';
 }
 </script>
 
 <template>
-  <div class="container">
-    <header class="p-5 mb-4 mt-4 rounded-3">
-      <h1>Minhas tarefas</h1>
-      <p>
-        Você possui {{ getTarefasPendentes().length }} tarefas pendentes
-      </p>
-    </header>
-    <form @submit.prevent="cadastraTarefa">
-      <div class="row">
-        <div class="col">
-          <input :value="estado.tarefaTemp" @change="evento => estado.tarefaTemp = evento.target.value" required type="text" placeholder="Digite a descrição da tarefa" class="form-control">
-        </div>
-        <div class="col-md-2">
-          <button type="submit" class="btn btn-primary">Cadastrar</button>
-        </div>
-        <div class="col-md-2">
-          <select @change="evento => estado.filtro = evento.target.value" class="form-control">
-            <option value="todas">Todas tarefas</option>
-            <option value="pendentes">Pendentes</option>
-            <option value="concluidas">Concluídas</option>
-          </select>
-        </div>
-      </div>
-    </form>
-    <ul class="list-group mt-4">
-      <li class="list-group-item" v-for="tarefas in getTarefasFiltradas()">
-        <input @change="evento => tarefas.concluida = evento.target.checked" :checked="tarefas.concluida" :id="tarefas.titulo" type="checkbox">
-        <label :class="{done: tarefas.concluida}" class="ms-3" :for="tarefas.titulo">
-          {{tarefas.titulo }}
-        </label>
-      </li>
-    </ul>
-  </div>
+<div class="container">
+<Cabecalho :tarefas-pendentes="getTarefasPendentes().length"/>
+<Formulario :trocar-filtro="evento => estado.filtro=evento.target.value" :tarefa-temp="estado.tarefaTemp" :edita-tarefa-temp="evento => estado.tarefaTemp= evento.target.value" :cadastraTarefa/>
+<ListaDeTarefas :tarefas="getTarefasFiltradas()"/>
+</div>
 </template>
 
-<style scoped>
-  .container {
-    background: url(https://images.unsplash.com/photo-1484480974693-6ca0a78fb36b?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D);
-    background-size: cover;
-    margin: 1rem 2rem;
-    opacity: 0.8;  
-    text-align: center;
-    color: black;
-    font-weight: 700;
-    display: block;
-  }
-  
-  .done {
-    text-decoration: line-through;
-    color: rgb(10, 211, 10);
-  }
-</style>
